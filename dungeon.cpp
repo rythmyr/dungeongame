80098644090d78bf7bf5e88ce680db0ab66d0ef7
Rythmyr <rythmyr@gmail.com>
2021-03-06 16:20:28 -0600

    don't place room if colliding with existing room

--- dungeon.cpp
+++ dungeon.cpp
@@ -21,35 +21,37 @@ void Dungeon::Generate(int x, int y) {
     this->_map = new char[x * y];
 
     for (int i = 0; i < x * y; i++) {
-        this->_map[i] = '.';
+        this->_map[i] = '#';
     }
 
-    for (; x >= 0; x-- ) {
-        this->SetTile(x, 0, '#');
-        this->SetTile(x, this->_sizeY - 1, '#');
-    }
-    for (; y >= 0; y-- ) {
-        this->SetTile(0, y, '#');
-        this->SetTile(this->_sizeX - 1, y, '#');
-    }
-
-    this->_playerX = (rand() % (this->_sizeX - 2)) + 1;
-    this->_playerY = (rand() % (this->_sizeY - 2)) + 1;
-
-    // try to place rooms 10 times (number is arbitrary)
-    for (int i = 0; i < 10; i ++) {
+    // try to place rooms 100 times
+    // number is arbitrary, higher numbers means more attempts, not necessarily more rooms
+    for (int i = 0; i < 100; i ++) {
+        bool skip = false;
         Room room;
         room.w = rand() % 5 + 3;
         room.h = rand() % 5 + 3;
         room.x = (rand() % (this->_sizeX - room.w - 4)) + 2;
         room.y = (rand() % (this->_sizeY - room.h - 4)) + 2;
+
+        for (unsigned int j = 0; j < this->_rooms.size(); j++) {
+            if (room.Collides(this->_rooms[j], 3)) {
+                skip = true;
+            }
+        }
+        if (skip) {continue;}
         for (int _x = room.x; _x < room.x + room.w; _x++) {
             for (int _y = room.y; _y < room.y + room.h; _y++) {
-                this->SetTile(_x, _y, '#');
+                this->SetTile(_x, _y, '.');
             }
         }
         this->_rooms.push_back(room);
     }
+
+    Room startRoom = this->_rooms[0];
+
+    this->_playerX = startRoom.x + (startRoom.w / 2);
+    this->_playerY = startRoom.y + (startRoom.h / 2);
 
     //TODO monster spawning, this random count probably isn't great
     //this->_monsterCount = (rand() % (this->_sizeX * this->_sizeY) / 30) + 10;
@@ -88,21 +90,6 @@ void Dungeon::Display() {
         Room& r = this->_rooms[i];
         mvprintw(this->_sizeY + i + 1, 0, "x: %d, y: %d, w: %d, h: %d", r.x, r.y, r.w, r.h);
     }
-
-    Room r1, r2;
-
-    r1.x = 0;
-    r1.y = 0;
-    r1.w = 6;
-    r1.h = 15;
-
-    r2.x = 10;
-    r2.y = 10;
-    r2.w = 10;
-    r2.h = 10;
-
-    //TODO debug info
-    mvaddch(this->_sizeY, this->_sizeX + 5, r1.Collides(r2, 5) ? 'c' : 'n');
 
     // move cursor out of the way
     // TODO turn off cursor, if possible
